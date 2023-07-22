392e71ef744ecd5cca2b1caaf39c7f873ab00f2a
TheOnlineCat <116917320+TheOnlineCat@users.noreply.github.com>
2023-07-23 01:44:44 +0800

    Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class BoomArm : MonoBehaviour
 {
+    public Camera cam;
+    public int speed = 50;
+    public int speedStrafe = 50;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovementControl();
+    }
+    
+    void MovementControl()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
         
+        Vector3 movement = new Vector3(speed * inputX, 0, speedStrafe * inputY);
+
+        movement *= Time.deltaTime;
+
+        transform.rotation = cam.transform.rotation;
+        transform.Translate(movement);
     }
 }
